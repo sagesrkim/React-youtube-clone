b7ef7493eb5e81bd6f5a6983e181337aa5187c58
saerom <sagesaeromkim@gmail.com>
2023-01-26 23:15:51 +0900

    router경로 설정, 검색header 구현

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import SearchHeader from './components/SearchHeader';
+
+
+function App() {
+  return (
+    <>
+      <SearchHeader />
+      <Outlet />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { BsYoutube, BsSearch } from 'react-icons/bs';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+
+export default function SearchHeader() {
+    const { keyword } = useParams();
+    const navigate = useNavigate();
+    const [text, setText] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate(`/videos/${text}`);
+        setText('');
+    }
+    useEffect(() => setText(keyword || ''), [keyword]);
+    return (
+        <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+            <Link to='/' className='flex items-center'>
+                <BsYoutube className='text-3xl text-brand' />
+                <h1 className='font-bold ml-2 text-2xl '>YouTube</h1>
+            </Link>
+            <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+                <input className='w-7/12 p-2 pl-5 outline-none  bg-black text-lg text-gray-50 rounded-l-full' onChange={(e) => setText(e.target.value)} type="text" value={text} placeholder='Search' />
+                <button className='bg-zinc-600 px-4 rounded-r-full hover:brightness-150 cursor-pointer'>
+                    <BsSearch />
+                </button>
+            </form>
+        </header>
+    );
+}
+
